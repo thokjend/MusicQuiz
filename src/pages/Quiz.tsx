6ff0412ad4f9cf8e7b8b4300bbcd0c3772243004
thokjend <thokjend@online.no>
2024-09-26 18:32:54 +0200

    Update Quiz.tsx

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 
+//work in progress
 export default function Quiz() {
+  const [question, setQuestion] = useState([]);
+  const fetchQuestion = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/music/lyrics");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      setQuestion(result);
+      console.log(question);
+    } catch (error) {
+      console.log("Fetch error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestion();
+  }, []);
+
   return (
     <div className="quiz-background">
       <Header></Header>
